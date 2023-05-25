5b603fca6232e52e074a0e8feb95092ff0d2939e
Dennis Marcelo dos Santos <dennismarcelo15@gmail.com>
2023-05-25 08:10:18 -0300

    feature: added pge MyTechs in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Home } from './Pages/Home';
 import { About } from './Pages/About';
+import { MyTechs } from './Pages/MyTechs';
 
 export function App() {
   return (
     <>
       <Home />
       <About />
+      <MyTechs />
     </>
   );
 }
